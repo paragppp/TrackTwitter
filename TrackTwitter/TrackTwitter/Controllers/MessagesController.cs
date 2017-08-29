721d75c5622d1adbccc819a61053226d989b4361
Parag Paithankar <parag.paithankar@gmail.com>
2017-08-29 16:14:55 -0700

    added commands and created basic app framework

--- TrackTwitter/TrackTwitter/Controllers/MessagesController.cs
+++ TrackTwitter/TrackTwitter/Controllers/MessagesController.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Linq;
-using System.Net;
+﻿using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
-using System.Web.Http.Description;
+using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
-using Newtonsoft.Json;
+using Microsoft.Bot.Connector.Teams;
+using Microsoft.Bot.Connector.Teams.Models;
 
 namespace TrackTwitter
 {
@@ -21,34 +20,61 @@ namespace TrackTwitter
         {
             if (activity.Type == ActivityTypes.Message)
             {
-                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                // calculate something for us to return
-                int length = (activity.Text ?? string.Empty).Length;
+                await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+            }
+            else if (activity.Type == ActivityTypes.Invoke)
+            {
+                //Compose extensions come in as Invokes.  Leverage the Teams SDK helper functions
+                if (activity.IsComposeExtensionQuery())
+                {
+                    // Determine the response object to reply with
+                    var invokeResponse = new ComposeExtension(activity).CreateComposeExtensionResponse();
 
-                // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
-                await connector.Conversations.ReplyToActivityAsync(reply);
+                    // Return the response
+                    return Request.CreateResponse<ComposeExtensionResponse>(HttpStatusCode.OK, invokeResponse);
+                }
+                else
+                {
+                    // Handle other types of Invoke activities here, e.g. CardActions
+                }
             }
             else
             {
-                HandleSystemMessage(activity);
+                await HandleSystemMessage(activity);
             }
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
         }
 
-        private Activity HandleSystemMessage(Activity message)
+        private async Task HandleSystemMessage(Activity message)
         {
-            if (message.Type == ActivityTypes.DeleteUserData)
-            {
-                // Implement user deletion here
-                // If we handle user deletion, return a real message
-            }
-            else if (message.Type == ActivityTypes.ConversationUpdate)
+            if (message.Type == ActivityTypes.ConversationUpdate)
             {
                 // Handle conversation state changes, like members being added and removed
                 // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                // Not available in all channels
+
+                // This event is called when the Bot is added too, so we can trigger a welcome message if the member added is the bot:
+                TeamEventBase eventData = message.GetConversationUpdateData();
+                if (eventData.EventType == TeamEventType.MembersAdded)
+                {
+                    for (int i = 0; i < message.MembersAdded.Count; i++)
+                    {
+                        //Check to see if the member added was the bot itself.  We're leveraging the fact that the inbound payload's Recipient is the bot.
+                        if (message.MembersAdded[i].Id == message.Recipient.Id)
+                        {
+                            // We'll use normal message parsing to display the welcome message.
+                            message.Text = "welcome";
+                            await Conversation.SendAsync(message, () => new Dialogs.RootDialog());
+
+                            break;
+                        }
+                    }
+                }
+            }
+            else if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
             }
             else if (message.Type == ActivityTypes.ContactRelationUpdate)
             {
@@ -62,8 +88,6 @@ namespace TrackTwitter
             else if (message.Type == ActivityTypes.Ping)
             {
             }
-
-            return null;
         }
     }
 }
